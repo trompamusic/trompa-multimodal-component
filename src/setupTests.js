119061887ef513831c49a22e29ad61d83978418d
demmyhonore <demmyhonore@gmail.com>
2021-02-25 14:00:19 +0100

    feat(project): remove redundant test setup

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-require('./requestAnimationFrame');
-
-const enzyme  = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-enzyme.configure({ adapter: new Adapter() });
--- src/testUtils.jsx
+++ src/testUtils.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
-import 'url-search-params-polyfill';
 import { getApolloClient } from './client';
 
 const client = getApolloClient('https://api-test.trompamusic.eu');
